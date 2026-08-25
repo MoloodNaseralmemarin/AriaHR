fe4be01aead405643cb156c8a980b3c8401dc28a
Melodi <moloodNaseralmemarin@gmail.com>
2026-08-25 08:36:32 -0700

    Remove Email Propertiy By Melodi

--- src/Modules/Organization/Domain/Entities/Organization.cs
+++ src/Modules/Organization/Domain/Entities/Organization.cs
@@ -11,7 +11,6 @@ public class Organization : BaseEntity
     public string Code { get; set; } = string.Empty;
     public string? NationalIdentifier { get; set; }
     public string? Phone { get; set; }
-    public string? Email { get; set; }
     public string? Address { get; set; }
     public bool IsActive { get; set; }
 
